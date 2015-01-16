a4252bb9fec316dea0542baaa0ccbf8d2cd8ff71
Angelo Calub <angelo.calub@macys.com>
2015-01-15 18:46:15 -0800

    adding latest documentation

--- index.js
+++ index.js
@@ -10,27 +10,47 @@ module.exports = function(json){
 	var killswitches = typeof json === "string" ? JSON.stringify(json) : json;
 
 
-
-
-	this.get = function(key, info){
+	this.getBoolean = function(key, info){
 		var obj = killswitches[key];
 		if(!obj){
-			throw new Error("killswitch.get(key) failed. no matching killswitch key found in killswitches.json.");
+			throw new Error("killswitch.getBoolean(key) failed. no matching killswitch key found in killswitches.json.");
 		}
 		else{
 			if(obj.enabled){
-				if(obj.targets && obj.targets.length > 0 && obj.targets.indexOf(info.name > -1)){
-					return obj.enabledValue;
+				if(obj.targets && obj.targets.length > 0 && obj.targets.indexOf(info.name) > -1){
+					return true;
 				}
 				else{
-					return obj.disabledValue;
+					return false;
 				}
 
 			}
 			else{
-				return obj.disabledValue;
+				return false;
 			}
 		}
+	};
+
+	this.getValue = function(key, info){
+		var flag = this.getBoolean(key, info);
+		var obj = this.get(key, info);
+		console.log('flag: ' + flag);
+		console.log(obj);
+		if(obj.enabledValue && obj.disabledValue && flag){
+			return obj.enabledValue;
+		}
+		else if(obj.enabledValue && obj.disabledValue && !flag){
+			return obj.disabledValue;
+		}
+		else{
+			return flag;
+		}
+	}
+			
+
+
+	this.get = function(key, info){
+		return this.getBoolean(key, info) ? killswitches[key] : null;
 	};
 }
 
--- test/core.js
+++ test/core.js
@@ -5,26 +5,52 @@ var	assert		= require("assert"),
 	killswitches	= require(__dirname + "/test-killswitches.json"),
 	ks;
 
-suite("fds-footer Test Suite", function(){
-	describe('get("key", info)', function(){
-		beforeEach(function(){
-			ks = new Killswitch(killswitches);
-		});
-		afterEach(function(){
-			ks = null;
-		});
-
+suite("Killswitch Test Suite", function(){
+	beforeEach(function(){
+		ks = new Killswitch(killswitches);
+	});
+	afterEach(function(){
+		ks = null;
+	});
+	describe('getBoolean(key, info)', function(){
 		it('should exist', function(){
-			assert.ok(typeof ks.get, 'function');
+			assert.ok(typeof ks.getBoolean, 'function');
 		});
-		it('should return enabled value', function(){
-			var rc = new RequestContext({params:{arg1:'api',arg2:'1',arg3:'product',arg4:'availability',arg5:'123,456,678'}, headers:{'user-agent':'tablet'}});
-			assert.equal(ks.get('newFooterCopyright', {name:'foo'}), false);
-			assert.ok(ks.get('newHeaderFOBInteraction',{name:'newHeader'}), "xyz");
+		it('should throw an error if killswitch definiton is not found', function(){
+			assert.ok(false);
+		});
+		it('should return correct boolean value when enabled', function(){
+			assert.equal(ks.getBoolean('f2', {name:'foo'}), true);
+			assert.equal(ks.getBoolean('f5', {name:'moduleX'}), true);
+
+		});
+		it('should not restrict any modules when wildcard "*" is used in targets', function(){
+			assert.equal(ks.getBoolean('f4', {name:'someModule'}), true);
+			assert.equal(ks.getBoolean('f4', {name:'fooModule'}), true);
+			assert.equal(ks.getBoolean('f4', {name:'foo'}), true);
+
 		});
 
-		it('should restrict usage if restrictByName array has module listed', function(){
-			assert.ok(ks.get('newHeaderFOBInteraction',{name:'newHeader'}), "xyz");
+		it('should return correct boolean value when disabled', function(){
+			assert.equal(ks.getBoolean('f1', {name:'foo'}), false);
+			assert.equal(ks.getBoolean('f1', {name:'bar'}), false);
+		});
+	});
+	describe('getValue(key, info)', function(){
+		it('should exist', function(){
+			assert.ok(typeof ks.getValue, 'function');
+		});
+		it("should return correct value when enabled", function(){
+			assert.equal(ks.getValue('f5', {name:'moduleX'}), 'f5-enabled');
+
+		});
+		
+	});
+
+	describe('get(key, info)', function(){
+		it("should return correct killswitch definition object when enabled", function(){
+			assert.ok(typeof ks.get, 'function');
+			assert.ok(ks.get("f5",{name:"moduleX"}).description === "test killswitch five");
 		});
 	});
 });
